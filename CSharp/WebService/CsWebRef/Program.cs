0776ed5a9ba77e59f571609d3d6b26bb23c9a19a
liuwake <iwake@live.cn>
2019-12-23 15:54:21 +0800

    start and success CsWebRef

---
+++ CSharp/WebService/CsWebRef/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsWebRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRef.WebService1 webService = new WebRef.WebService1();
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(webService.HelloWorld());
+            Console.WriteLine(a + "+" + b + "=" + webService.Add(a, b));
+            Console.ReadKey();
+
+        }
+    }
+}
