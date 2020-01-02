87aea3684bb20c647314b62d1d011f1c545474fb
WK-HS <iwake@live.cn>
2020-01-02 11:32:06 +0800

    up

---
+++ CSharp/CsConsole/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsConsole
+{
+    class Class1
+    {
+        public static void function()
+        {
+            Console.WriteLine("----Invoke Class1.function()-OK---");
+            return;
+        }
+    }
+}
