d541cd9d9f6b7eedc560cab9549c0551f08e9028
WK-HS <iwake@live.cn>
2020-01-09 17:19:30 +0800

    CsWfDb add function

--- CSharp/CsWf/CsWfDb/Form1.Designer.cs
+++ CSharp/CsWf/CsWfDb/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -44,6 +45,28 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.accessDemoDataSet = new CsWfDb.AccessDemoDataSet();
+            this.scanBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.scanTableAdapter = new CsWfDb.AccessDemoDataSetTableAdapters.ScanTableAdapter();
+            this.dbIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hospitalNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bedNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tagCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.signDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.passDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDemoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scanBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -88,7 +111,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(240, 210);
+            this.button5.Location = new System.Drawing.Point(164, 210);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(146, 54);
             this.button5.TabIndex = 4;
@@ -98,23 +121,23 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(240, 23);
+            this.textBox1.Location = new System.Drawing.Point(225, 62);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 21);
+            this.textBox1.Size = new System.Drawing.Size(82, 21);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(240, 50);
+            this.textBox2.Location = new System.Drawing.Point(225, 89);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 21);
+            this.textBox2.Size = new System.Drawing.Size(82, 21);
             this.textBox2.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(175, 26);
+            this.label1.Location = new System.Drawing.Point(160, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 7;
@@ -123,7 +146,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(177, 58);
+            this.label2.Location = new System.Drawing.Point(162, 97);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 8;
@@ -132,7 +155,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(177, 85);
+            this.label3.Location = new System.Drawing.Point(162, 124);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 10;
@@ -140,15 +163,15 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(240, 77);
+            this.textBox3.Location = new System.Drawing.Point(225, 116);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(146, 21);
+            this.textBox3.Size = new System.Drawing.Size(82, 21);
             this.textBox3.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(177, 112);
+            this.label4.Location = new System.Drawing.Point(162, 151);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(47, 12);
             this.label4.TabIndex = 12;
@@ -156,15 +179,15 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(240, 104);
+            this.textBox4.Location = new System.Drawing.Point(225, 143);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(146, 21);
+            this.textBox4.Size = new System.Drawing.Size(82, 21);
             this.textBox4.TabIndex = 11;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(177, 139);
+            this.label5.Location = new System.Drawing.Point(162, 178);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 12);
             this.label5.TabIndex = 14;
@@ -172,14 +195,14 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(240, 131);
+            this.textBox5.Location = new System.Drawing.Point(225, 170);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(146, 21);
+            this.textBox5.Size = new System.Drawing.Size(82, 21);
             this.textBox5.TabIndex = 13;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(240, 270);
+            this.button6.Location = new System.Drawing.Point(164, 270);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(146, 54);
             this.button6.TabIndex = 15;
@@ -187,11 +210,166 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(316, 210);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 54);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Command: select Scan";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(164, 330);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(146, 54);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Command: insert unique Scan";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 12);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Insert or select";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(316, 270);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(146, 54);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "DataReader: select Scan";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(339, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(339, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "select Results:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dbIdDataGridViewTextBoxColumn,
+            this.hospitalNoDataGridViewTextBoxColumn,
+            this.bedNoDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.tagCodeDataGridViewTextBoxColumn,
+            this.scanDateDataGridViewTextBoxColumn,
+            this.signDataGridViewCheckBoxColumn,
+            this.passDataGridViewCheckBoxColumn});
+            this.dataGridView1.DataSource = this.scanBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(475, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(604, 430);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // accessDemoDataSet
+            // 
+            this.accessDemoDataSet.DataSetName = "AccessDemoDataSet";
+            this.accessDemoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // scanBindingSource
+            // 
+            this.scanBindingSource.DataMember = "Scan";
+            this.scanBindingSource.DataSource = this.accessDemoDataSet;
+            this.scanBindingSource.CurrentChanged += new System.EventHandler(this.scanBindingSource_CurrentChanged);
+            // 
+            // scanTableAdapter
+            // 
+            this.scanTableAdapter.ClearBeforeFill = true;
+            // 
+            // dbIdDataGridViewTextBoxColumn
+            // 
+            this.dbIdDataGridViewTextBoxColumn.DataPropertyName = "DbId";
+            this.dbIdDataGridViewTextBoxColumn.HeaderText = "DbId";
+            this.dbIdDataGridViewTextBoxColumn.Name = "dbIdDataGridViewTextBoxColumn";
+            // 
+            // hospitalNoDataGridViewTextBoxColumn
+            // 
+            this.hospitalNoDataGridViewTextBoxColumn.DataPropertyName = "HospitalNo";
+            this.hospitalNoDataGridViewTextBoxColumn.HeaderText = "HospitalNo";
+            this.hospitalNoDataGridViewTextBoxColumn.Name = "hospitalNoDataGridViewTextBoxColumn";
+            // 
+            // bedNoDataGridViewTextBoxColumn
+            // 
+            this.bedNoDataGridViewTextBoxColumn.DataPropertyName = "BedNo";
+            this.bedNoDataGridViewTextBoxColumn.HeaderText = "BedNo";
+            this.bedNoDataGridViewTextBoxColumn.Name = "bedNoDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "Age";
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            // 
+            // tagCodeDataGridViewTextBoxColumn
+            // 
+            this.tagCodeDataGridViewTextBoxColumn.DataPropertyName = "TagCode";
+            this.tagCodeDataGridViewTextBoxColumn.HeaderText = "TagCode";
+            this.tagCodeDataGridViewTextBoxColumn.Name = "tagCodeDataGridViewTextBoxColumn";
+            // 
+            // scanDateDataGridViewTextBoxColumn
+            // 
+            this.scanDateDataGridViewTextBoxColumn.DataPropertyName = "ScanDate";
+            this.scanDateDataGridViewTextBoxColumn.HeaderText = "ScanDate";
+            this.scanDateDataGridViewTextBoxColumn.Name = "scanDateDataGridViewTextBoxColumn";
+            // 
+            // signDataGridViewCheckBoxColumn
+            // 
+            this.signDataGridViewCheckBoxColumn.DataPropertyName = "Sign";
+            this.signDataGridViewCheckBoxColumn.HeaderText = "Sign";
+            this.signDataGridViewCheckBoxColumn.Name = "signDataGridViewCheckBoxColumn";
+            // 
+            // passDataGridViewCheckBoxColumn
+            // 
+            this.passDataGridViewCheckBoxColumn.DataPropertyName = "Pass";
+            this.passDataGridViewCheckBoxColumn.HeaderText = "Pass";
+            this.passDataGridViewCheckBoxColumn.Name = "passDataGridViewCheckBoxColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1091, 461);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
@@ -211,6 +389,9 @@
             this.Name = "Form1";
             this.Text = "WfCsDb";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDemoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scanBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,6 +415,25 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private AccessDemoDataSet accessDemoDataSet;
+        private System.Windows.Forms.BindingSource scanBindingSource;
+        private AccessDemoDataSetTableAdapters.ScanTableAdapter scanTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dbIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hospitalNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bedNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tagCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn signDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn passDataGridViewCheckBoxColumn;
     }
 }
 
--- CSharp/CsWf/CsWfDb/Form1.cs
+++ CSharp/CsWf/CsWfDb/Form1.cs
@@ -54,6 +54,8 @@ namespace CsWfDb
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: 这行代码将数据加载到表“accessDemoDataSet.Scan”中。您可以根据需要移动或删除它。
+            this.scanTableAdapter.Fill(this.accessDemoDataSet.Scan);
 
         }
 
@@ -212,6 +214,159 @@ namespace CsWfDb
                     conn.Close();
                 }
             }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //编写数据库连接串
+            string connStr = oledbConnDefaultStr;
+            //创建SQLConnection的实例
+            OleDbConnection conn = null;
+            try
+            {
+                conn = new OleDbConnection(connStr);
+                //打开数据库连接
+                conn.Open();
+                string sql = "Select count(*) from Scan where HospitalNo='{0}'";
+                //填充SQL语句
+                sql = string.Format(sql, textBox1.Text);
+                //创建SqlCommand对象
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                //执行SQL语句
+                int returnvalue = (int)cmd.ExecuteScalar();
+                //判断SQL语句是否执行成功
+                if (returnvalue != 0)
+                {
+                    MessageBox.Show("查询存在！");
+                }
+                else
+                {
+                    MessageBox.Show("查询不存在！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询失败！" + ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    //关闭数据库连接
+                    conn.Close();
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //编写数据库连接串
+            string connStr = oledbConnDefaultStr;
+            //创建 SqlConnection的实例
+            OleDbConnection conn = null;
+            try
+            {
+                conn = new OleDbConnection(connStr);
+                //打开数据库连接
+                conn.Open();
+                string sql = "insert into Scan(HospitalNo, BedNo,Name, Tagcode, Sign) values('{0}','{1}','{2}','{3}','{4}')";
+                //判断用户名是否重复
+                {
+                    string checkHoIdSql = "select count(*) from Scan where HospitalNo='{0}'";
+                    checkHoIdSql = string.Format(checkHoIdSql, textBox1.Text);
+                    //创建SqlCommand对象
+                    OleDbCommand cmdCheckHoId = new OleDbCommand(checkHoIdSql, conn);
+                    //执行SQL语句
+                    int isRepeatName = (int)cmdCheckHoId.ExecuteScalar();
+                    if (isRepeatName != 0)
+                    {
+                        //用户名重复，则不执行注册操作
+                        MessageBox.Show("住院号已存在！");
+                        return;
+                    }
+                }
+                //填充SQL语句
+                sql = string.Format(sql, textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+                //创建SqlCommand对象
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                //执行SQL语句
+                int returnvalue = cmd.ExecuteNonQuery();
+                //判断SQL语句是否执行成功
+                if (returnvalue != -1)
+                {
+                    MessageBox.Show("扫描添加成功！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("扫描添加失败！" + ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    //关闭数据库连接
+                    conn.Close();
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //编写数据库连接串
+            string connStr = oledbConnDefaultStr;
+            //创建 SqlConnection的实例
+            OleDbConnection conn = null;
+            //定义SqlDataReader类的对象
+            OleDbDataReader dr = null;
+            try
+            {
+                conn = new OleDbConnection(connStr);
+                //打开数据库连接
+                conn.Open();
+                string sql = "select HospitalNo,Name from Scan where HospitalNo='{0}'";
+                //填充SQL语句
+                sql = string.Format(sql, textBox1.Text);
+                //创建SqlCommand对象
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                //执行Sql语句
+                dr = cmd.ExecuteReader();
+                //判断SQL语句是否执行成功
+                if (dr.Read())
+                {
+                    //读取指定用户名对应的用户编号和密码
+                    string msg = "HoId：" + dr[0] + " Name：" + dr[1];
+                    //将msg的值显示在标签上
+                    label7.Text = msg;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询失败！" + ex.Message);
+            }
+            finally
+            {
+                if (dr != null)
+                {
+                    //判断dr不为空，关闭OleDbDataReader对象
+                    dr.Close();
+                }
+                if (conn != null)
+                {
+                    //关闭数据库连接
+                    conn.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void scanBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
