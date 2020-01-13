6d7fde26dc148ee5fb0b2e725659b9f92a17b9f3
WK-HS <iwake@live.cn>
2020-01-13 15:06:46 +0800

    update

--- HalCs/HalCsWf/HalCsWfDemo/Form1.Designer.cs
+++ HalCs/HalCsWf/HalCsWfDemo/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(18, 19);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(291, 390);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(291, 390);
+            this.hWindowControl1.HMouseMove += new HalconDotNet.HMouseEventHandler(this.hWindowControl1_HMouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- HalCs/HalCsWf/HalCsWfDemo/Form1.cs
+++ HalCs/HalCsWf/HalCsWfDemo/Form1.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Halcon
+using HalconDotNet;//使用命名空间
 
 namespace HalCsWfDemo
 {
     public partial class Form1 : Form
     {
+
+        //Data
+        
+        //Halcon Data
+        HObject ho_Demo, ho_GrayImage;
+            
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HOperatorSet.GenEmptyObj(out ho_GrayImage);
+                ho_GrayImage.Dispose();
+                HOperatorSet.Rgb1ToGray(ho_Demo, out ho_GrayImage);
+                HOperatorSet.DispObj(ho_GrayImage, hWindowControl1.HalconWindow);
+                ho_Demo.Dispose(); //释放ho_Demo的内存
+                ho_GrayImage.Dispose();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void hWindowControl1_HMouseMove(object sender, HMouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HOperatorSet.GenEmptyObj(out ho_Demo);//创建对象
+            ho_Demo.Dispose();//清理内存，使得ho_Demo中空无一物
+            HOperatorSet.ReadImage(out ho_Demo, "C:/demo.jpg");//读入内存中
+            HOperatorSet.DispObj(ho_Demo, hWindowControl1.HalconWindow);//显示在HWindowControl控件中
+        }
+
+       
     }
 }
