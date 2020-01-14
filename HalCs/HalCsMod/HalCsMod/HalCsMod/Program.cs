fbf0db1fb79fb47a5f33efab0b8a2218206aed82
WK-HS <iwake@live.cn>
2020-01-14 13:46:54 +0800

    start HalCsMod

--- HalCs/HalCsMod/HalCsMod/HalCsMod/Program.cs
+++ HalCs/HalCsMod/HalCsMod/HalCsMod/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//
+using HalconDotNet;
 
 namespace HalCsMod
 {
@@ -11,7 +13,25 @@ namespace HalCsMod
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+           try
+            {
+                HalMain();
+
+            }
+            catch(HalconException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadKey();
+        }
+        static void HalMain()
+        {
+            HalCam halCam = new HalCam();
+            halCam.Cam();
+            halCam.Vision(out HTuple hv_Mean, out HTuple hv_Deviation);
+            Console.WriteLine("halEn = " + halCam.halEn);
+            Console.WriteLine("Image mean= " + hv_Mean.D);
+            Console.WriteLine(halCam.ho_Image.GetType());
         }
     }
 }
