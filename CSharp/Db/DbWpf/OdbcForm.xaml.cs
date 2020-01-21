0b89a7538b9a47b2a858c28879817f79eaf22782
WK-HS <iwake@live.cn>
2020-01-21 11:20:45 +0800

    Fix Update Bug

--- CSharp/Db/DbWpf/OdbcForm.xaml.cs
+++ CSharp/Db/DbWpf/OdbcForm.xaml.cs
@@ -22,15 +22,17 @@ namespace DbWpf
     /// </summary>
     public partial class OdbcForm : Window
     {
-        DataSet ds = new DataSet();
-        string sql = @"Select * from Used";
-        OdbcConnection connection;
+        //DataSet ds = new DataSet();
+        //string sql = @"Select * from Used";
+        //OdbcConnection connection;
+        //DataView dataView;
+        //OdbcDataAdapter da;
 
         public OdbcForm()
         {
             //Init
             InitializeComponent();
-            UsedInit();
+            //UsedInit();
         }
         // Wpf
         private void ButtonUsedUpdate_Click(object sender, RoutedEventArgs e)
@@ -39,41 +41,46 @@ namespace DbWpf
         }
 
         //Func Sql
-        public void UsedInit()
-        {
-            //string connString = "Driver={Microsoft Access Driver (*.mdb)};" + "Dbq=C:\\WK\\Db\\Used.mdb;";
-            string connString = "Dsn=Used"; 
-            this.connection = new OdbcConnection(connString);//THIS!
-        }
+        //public void UsedInit()
+        //{
+        //    //string connString = "Driver={Microsoft Access Driver (*.mdb)};" + "Dbq=C:\\WK\\Db\\Used.mdb;";
+        //    string connString = "Dsn=Used"; 
+        //    this.connection = new OdbcConnection(connString);//THIS!
+        //}
         public void UsedSaveChange()//save 功能bug
         {
-            OdbcDataAdapter da = new OdbcDataAdapter();
-            da.SelectCommand = new OdbcCommand(sql, connection);
-            OdbcCommandBuilder cb = new OdbcCommandBuilder(da);
-            DataTable dt = new DataTable();
-            dt = ((DataView)dataGridResult.ItemsSource).Table;
-            da.Update(ds.Tables["Used"]);  //DataGrid和ds的table绑定之后，DataGrid的更改会自动更新到Dataset
+            string sql = @"Select * from Used";
+            string connString = "Dsn=Used";
+            OdbcConnection connection = new OdbcConnection(connString);
+            OdbcDataAdapter dataAdapter = new OdbcDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
+            DataSet dataSet = new DataSet();
+
+            OdbcCommandBuilder commandBuilder = new OdbcCommandBuilder(dataAdapter);
+            DataTable dataTable = new DataTable();
+            dataTable = ((DataView)dataGridResult.ItemsSource).Table;
+            dataAdapter.Update(dataSet.Tables["Used"]);  //DataGrid和ds的table绑定之后，DataGrid的更改会自动更新到Dataset
         }
         public void UsedUpdateData()
         {
-            OdbcDataAdapter da = new OdbcDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
-            da.Fill(ds, "Used");  //从数据库中读取数据，并填充ds
-            DataView dv = new DataView(ds.Tables["Used"]); //创建DataView实例dv，并指定其DataTable
-            dataGridResult.ItemsSource = dv;  //设置DataGrid的ItemsSource属性
-        }
-        public void UsedUpdateCode()
-        {
-            //
-            //OdbcConnection connection = new OdbcConnection(connString);
-            ////string sql = @"Select * from Used";
-            ////DataSet ds = new DataSet();
-            //OdbcDataAdapter da = new OdbcDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
+            
+            string sql = @"Select * from Used";
+            string connString = "Dsn=Used";
+            OdbcConnection connection = new OdbcConnection(connString);
+            OdbcDataAdapter dataAdapter = new OdbcDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, "Used");  //从数据库中读取数据，并填充ds
 
-            //da.Fill(ds, "Used");  //从数据库中读取数据，并填充ds
-            //DataView dv = new DataView(ds.Tables["Used"]); //创建DataView实例dv，并指定其DataTable
-            //dataGridResult.ItemsSource = dv;  //设置DataGrid的ItemsSource属性
-
+            DataView  dataView = new DataView(dataSet.Tables["Used"]); //创建DataView实例dv，并指定其DataTable
+            dataGridResult.ItemsSource = dataView;  //设置DataGrid的ItemsSource属性
         }
+        //public void UsedUpdateOnece()
+        //{
+        //    OdbcDataAdapter da = new OdbcDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
+        //    da.Fill(ds, "Used");  //从数据库中读取数据，并填充ds
+        //    this.dataView = new DataView(ds.Tables["Used"]); //创建DataView实例dv，并指定其DataTable
+        //    dataGridResult.ItemsSource = dataView;  //设置DataGrid的ItemsSource属性
+
+        //}
 
     }
 }
