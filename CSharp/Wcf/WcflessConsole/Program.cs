fe88e8cd3a53137b9ad2417e7eb94202a50c4321
WK-HS <iwake@live.cn>
2020-02-13 16:02:30 +0800

    start WcflessConsole

---
+++ CSharp/Wcf/WcflessConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcflessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            //string testString = Comm.GetImage();
+            Console.WriteLine(Comm.GetImage());
+            Console.ReadKey();
+        }
+    }
+}
