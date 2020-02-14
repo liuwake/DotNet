4b5599f6d60b9423766c05045d8b8245df0a2adf
WK-HS <iwake@live.cn>
2020-02-14 11:10:43 +0800

    start WcfClass

---
+++ CSharp/Wcf/WcfClass/WcfClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//AI System File IO
+
+namespace WcfClass
+{
+    class WcfClass
+    {
+               
+        private int InvokeWebservice(string url, string @namespace, string classname, string methodname, object[] args)
+        {
+            try
+            {
+                System.Net.WebClient wc = new System.Net.WebClient();
+                System.IO.Stream stream = wc.OpenRead(url);
+                System.Web.Services.Description.ServiceDescription sd = System.Web.Services.Description.ServiceDescription.Read(stream);
+                System.Web.Services.Description.ServiceDescriptionImporter sdi = new System.Web.Services.Description.ServiceDescriptionImporter();
+                sdi.AddServiceDescription(sd, "", "");
+                System.CodeDom.CodeNamespace cn = new System.CodeDom.CodeNamespace(@namespace);
+                System.CodeDom.CodeCompileUnit ccu = new System.CodeDom.CodeCompileUnit();
+                ccu.Namespaces.Add(cn);
+                sdi.Import(cn, ccu);
+
+                Microsoft.CSharp.CSharpCodeProvider csc = new Microsoft.CSharp.CSharpCodeProvider();
+                System.CodeDom.Compiler.ICodeCompiler icc = csc.CreateCompiler();
+
+                System.CodeDom.Compiler.CompilerParameters cplist = new System.CodeDom.Compiler.CompilerParameters();
+                cplist.GenerateExecutable = false;
+                cplist.GenerateInMemory = true;
+                cplist.ReferencedAssemblies.Add("System.dll");
+                cplist.ReferencedAssemblies.Add("System.XML.dll");
+                cplist.ReferencedAssemblies.Add("System.Web.Services.dll");
+                cplist.ReferencedAssemblies.Add("System.Data.dll");
+
+                System.CodeDom.Compiler.CompilerResults cr = icc.CompileAssemblyFromDom(cplist, ccu);
+                if (true == cr.Errors.HasErrors)
+                {
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    foreach (System.CodeDom.Compiler.CompilerError ce in cr.Errors)
+                    {
+                        sb.Append(ce.ToString());
+                        sb.Append(System.Environment.NewLine);
+                    }
+                    throw new Exception(sb.ToString());
+                }
+                System.Reflection.Assembly assembly = cr.CompiledAssembly;
+                Type t = assembly.GetType(@namespace + "." + classname, true, true);
+                object obj = Activator.CreateInstance(t);
+                System.Reflection.MethodInfo mi = t.GetMethod(methodname);
+
+                mi.Invoke(obj, args);
+            }
+            catch
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+
+        private string ImageToBase64String(string ImagePath)
+        {
+            if (ImagePath == "")
+                return "";
+
+            byte[] buffer = new byte[0];
+            System.IO.FileInfo fi = new System.IO.FileInfo(ImagePath);
+            long len = fi.Length;
+            System.IO.FileStream fs = new System.IO.FileStream(ImagePath, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read);
+            buffer = new byte[len];
+            fs.Read(buffer, 0, (int)len);
+            fs.Close();
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        private void BytesToFile(byte[] bytes, string filePath)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    ms.WriteTo(fs);
+                }
+            }
+        }
+
+    }
+}
