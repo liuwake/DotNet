af6b42ed497ba2a6aa8d073bd43fd888380306ef
WK-T540P <iwake@live.cn>
2020-02-21 16:09:23 +0800

    Start CsFileShare

---
+++ CSharp/Comm/CsFileshare/CsFileshare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsFileshare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
