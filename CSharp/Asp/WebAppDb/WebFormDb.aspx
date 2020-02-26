b710bfd136d091be43f2cedc0b0e7cefbbae3cc4
WK-HS <iwake@live.cn>
2020-02-26 09:43:08 +0800

    Add WebFormDb: display Db locally

---
+++ CSharp/Asp/WebAppDb/WebFormDb.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormDb.aspx.cs" Inherits="WebAppDb.WebFormDb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Label" Width="200px"></asp:Label>
+        <div>
+
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ CSharp/Asp/WebAppDb/WebFormDb.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SQLite;
+
+
+namespace WebAppDb
+{
+    public partial class WebFormDb : System.Web.UI.Page
+    {
+        DataSet ds = new DataSet();
+        string sql = @"Select * from Used";
+        SQLiteConnection connection;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Page_Load");
+            UsedInit();
+            UsedUpdateData();
+        }
+
+        public void UsedInit()
+        {
+            string connString = "Data Source=C:\\WK\\Db\\Used.sqlite;Version=3";
+            this.connection = new SQLiteConnection(connString);//THIS!
+            System.Diagnostics.Debug.WriteLine("UsedInite");
+        }
+
+        public void UsedUpdateData()
+        {
+            SQLiteDataAdapter da = new SQLiteDataAdapter(sql, connection);  //创建SqlDataAdapter实例da，并指定SQL查询string和SqlConnection
+            da.Fill(ds, "Used");  //从数据库中读取数据，并填充ds
+            DataView dv = new DataView(ds.Tables["Used"]); //创建DataView实例dv，并指定其DataTable
+            GridView1.DataSource = dv;  //设置DataGrid的ItemsSource属性
+            GridView1.DataBind();
+            System.Diagnostics.Debug.WriteLine("UsedUpdateData");
+        }
+
+    }
+}
