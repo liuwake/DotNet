99b98ca560b56af0b8ccaa1fdf53e68aee68188d
WK-HS <iwake@live.cn>
2020-03-02 14:25:50 +0800

    Sort Proj

---
+++ CSharp/Wcf/WebApp/Db.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Db.aspx.cs" Inherits="WebApp.Db" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Height="229px" TextMode="MultiLine" Width="744px"></asp:TextBox>
+            <asp:GridView ID="GridView1" runat="server" Height="155px" Width="749px">
+            </asp:GridView>
+            <asp:GridView ID="GridView2" runat="server" Height="71px" Width="747px">
+            </asp:GridView>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="dbAll" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" style="height: 21px" Text="dbNew" />
+    </form>
+</body>
+</html>
---
+++ CSharp/Wcf/WebApp/Db.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SQLite;
+//using WebApplication3.WebRef;
+
+
+namespace WebApp
+{
+    public partial class Db: System.Web.UI.Page
+    {
+        WebRef.Service1 clientWcf = new WebRef.Service1();
+        WebRefDb.Service1 clientDb = new WebRefDb.Service1();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            this.TextBox1.Text = "No Web Test OK";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //this.TextBox1.Text = clientWcf.GetJson();
+            clientDb.openSql();
+            GridView1.DataSource = clientDb.querySql();
+            GridView1.DataBind();
+            //}
+            //catch(SQLiteException ex)
+            //{
+            //    this.Label1.Text = ex.ToString();
+            //}
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //this.TextBox1.Text = clientDb.GetHello();
+            clientDb.openSql();
+            GridView2.DataSource = clientDb.dbNew();
+            GridView2.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
