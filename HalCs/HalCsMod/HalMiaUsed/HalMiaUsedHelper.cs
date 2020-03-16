bf4de4c26a93ee591c81f24fe01453ecaa9cbc64
WK-HS <iwake@live.cn>
2020-03-16 17:22:33 +0800

    Add

---
+++ HalCs/HalCsMod/HalMiaUsed/HalMiaUsedHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using HalconDotNet;
+//using static HDevelopExport;
+
+
+
+namespace HalCsMod
+{
+    public partial class HalMiaUsedHelper
+    {
+        public void Convert()
+        {
+
+
+        }
+    }
+}
