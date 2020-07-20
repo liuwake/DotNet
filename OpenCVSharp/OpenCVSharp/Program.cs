12c18be068480458ad885b57471bf2519062c2e9
WK-HS <iwake@live.cn>
2020-07-20 16:46:14 +0800

    Update: Add function: display a image

--- OpenCVSharp/OpenCVSharp/Program.cs
+++ OpenCVSharp/OpenCVSharp/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenCvSharp;    //添加相应的引用即可
 
 namespace OpenCVSharp
 {
@@ -10,6 +10,9 @@ namespace OpenCVSharp
     {
         static void Main(string[] args)
         {
+            Mat source = new Mat(@"C:\WK\demo.jpg", ImreadModes.Color);
+            Cv2.ImShow("Demo", source);
+            Cv2.WaitKey(0);
         }
     }
 }
